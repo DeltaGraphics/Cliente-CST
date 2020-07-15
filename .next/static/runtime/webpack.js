5626ad668dea224fbe713f279ddfac73d7e12abd
Anny Moronta <annymorontajimenez@gmail.com>
2020-07-15 18:47:20 -0400

    feat: update .gitignore

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "6ad815902ca9b9b46890";
+/******/ 	var hotCurrentHash = "10ed47ae67bead08352b";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
